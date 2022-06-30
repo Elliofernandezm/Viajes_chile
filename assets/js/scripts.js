b11ef9ee4ae46ef02338e14ff7c345f2921bcfb0
Elliofernandezm <elliofernandez.m@gmail.com>
2022-06-29 23:45:40 -0400

    se agrega tooltiips

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(document).ready(function () {
+    $("#btn").click(function () {
+        const correo =  alert ("Tu Mensaje fue enviado")
+    })
+    
+})
