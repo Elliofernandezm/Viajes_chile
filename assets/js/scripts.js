de0f2d29495cf49742c1a54e669e2765bd9aca48
Elliofernandezm <elliofernandez.m@gmail.com>
2022-07-01 00:34:25 -0400

    trabajo en el type form y smooth scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,22 @@
+//scroll
+$(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      },800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
@@ -9,3 +28,5 @@ $(document).ready(function () {
     })
     
 })
+var
+
